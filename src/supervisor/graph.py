3e9a6dabc1f49a3862e4f8befadc2e22c9866596
dsu979 <haviethung300409@gmail.com>
2025-05-15 17:57:50 +0700

    Refactor graph.py to use InMemorySaver and InMemoryStore, and update app compilation with checkpoint and store

--- src/supervisor/graph.py
+++ src/supervisor/graph.py
@@ -2,7 +2,8 @@
 
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph_supervisor import create_supervisor
-from langgraph.checkpoint.memory import MemorySaver
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.store.memory import InMemoryStore
 from agent_nodes.notify_agent import notify_agent
 from agent_nodes.monitoring_agent import monitoring_agent
 from langchain.prompts import ChatPromptTemplate
@@ -19,6 +20,9 @@ llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"), model="gemini-
 monitoring_agent_instance = monitoring_agent()
 notify_agent_instance = notify_agent()
 
+checkpoint = InMemorySaver()
+store = InMemoryStore()
+
 graph = create_supervisor(
     [monitoring_agent_instance, notify_agent_instance],
     model=llm,
@@ -26,21 +30,8 @@ graph = create_supervisor(
         "You are a team supervisor managing a network device. "
         "For reporting and retrive information you can use the monitoring agent. "
         "For sending notifications you can use the notify agent about data when you have completed the task. "
-    )
+    ),
+    output_mode="full_history"
     )
 
-app = graph.compile()
-
-if __name__ == "__main__":
-    # Example messages to notify
-    while True:
-        user_input = input("Ask to AI Agent the question or 'exit' to quit: ")
-        inputs = {"messages": [{"role": "user", "content": user_input}]}
-        for s in app.stream(inputs, stream_mode="values"):
-            message = s["messages"][-1]
-            if isinstance(message, tuple):
-                print(message)
-            else:
-                message.pretty_print()
-        if user_input.lower() == "exit":
-            break
+app = graph.compile(checkpointer=checkpoint, store=store)
