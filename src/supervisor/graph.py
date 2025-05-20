fd773e3f20defc7df22f4d4bf6f61a8991f5e2df
Hestii <haviethung300409@gmail.com>
2025-05-20 17:49:27 +0700

    Refactor graph.py to update import statements and clean up unused variables; add new mcp_notify.py file for future notifications

--- src/supervisor/graph.py
+++ src/supervisor/graph.py
@@ -7,20 +7,14 @@ from langgraph.store.memory import InMemoryStore
 from mcp_src.mcp_client.mcp_healthcheck_agent import healcheck_agent
 from contextlib import asynccontextmanager
 
-from langchain.prompts import ChatPromptTemplate
+from langchain.prompts import PromptTemplate
 
 from dotenv import load_dotenv, find_dotenv
 import os
 
 load_dotenv(find_dotenv(), override=True)
-
-
-
 llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"), model="gemini-2.0-flash-lite")
-mcp_healthcheck_agent = healcheck_agent()
 
-# checkpoint = InMemorySaver()
-# store = InMemoryStore()
 
 @asynccontextmanager
 async def setup_supervisor_graph():
@@ -29,11 +23,7 @@ async def setup_supervisor_graph():
     using your specific `create_supervisor`.
     Returns the compiled supervisor application.
     """
-    global app
     async with healcheck_agent() as actual_mcp_healthcheck_agent:
-
-        checkpoint = InMemorySaver()
-
         # Using YOUR `create_supervisor` from `langgraph_supervisor`
         supervisor_definition = create_supervisor(
             agents=[actual_mcp_healthcheck_agent], # Correctly passing the resolved agent
