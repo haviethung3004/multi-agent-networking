0342e893d98120dfa0bb238bfdeedadf569f792a
Hestii <haviethung300409@gmail.com>
2025-05-22 17:48:50 +0700

    fix: improve prompt clarity in notify agent and ensure proper tool usage

--- src/mcp_src/mcp_client/mcp_notify_agent.py
+++ src/mcp_src/mcp_client/mcp_notify_agent.py
@@ -17,8 +17,7 @@ load_dotenv(find_dotenv(), override=True)
 
 prompt = """
     Your task is to send a message to the user by telegram.
-    You can use tool to do it
-    
+    Remember that you must use tool a time and wait for the answer.
     Question:
 
     {messages}
@@ -47,9 +46,11 @@ async def mcp_notify_agent():
             # Create the agent with the tools
             agent = create_react_agent(
                 tools=tools,
+                interrupt_after=["tools"],
                 model=llm,
                 name="notify-agent",
-                prompt=prompt_template
+                prompt=prompt_template,
+                
             )
             yield agent
 
