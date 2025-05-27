2a3c080aac29281e32ad35b14738a9fee7c91ce9
Hestii <haviethung300409@gmail.com>
2025-05-27 15:31:42 +0700

    fix: update .gitignore and langgraph.json for improved dependency management; add test_graph.py for supervisor agent implementation

---
+++ src/supervisor/test_graph.py
@@ -0,0 +1,172 @@
+from contextlib import asynccontextmanager
+import os
+from dotenv import load_dotenv, find_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langgraph.prebuilt import create_react_agent
+from langgraph_supervisor import create_supervisor
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain.prompts import PromptTemplate
+
+
+# Load environment variables
+load_dotenv(find_dotenv(), override=True)
+
+############################# Initialize the PROMPT AGENT #############################
+supervisor_prompt = """
+    You are a highly experienced CCIE (Cisco Certified Internetwork Expert) specializing managing a network team.
+    You are a Supervisor Agent in a multi-agent system designed to coordinate a team of specialized agents, some of which operate over a networked environment, 
+    to complete complex tasks efficiently. Your role is to interpret user requests, decompose them into subtasks, 
+    delegate tasks to appropriate agents (including those handling networking-related functions), 
+    manage network communication between agents, ensure fault tolerance, and synthesize results into a cohesive output.
+    When user ask you somthing, must understand the task and delegate the task to the appropriate agents.
+    If you not sure about task belong to IOS or ACI, you can ask the user for clarification.
+    Don't use agent if you are not sure about the task.
+
+    Please follow these steps to ensure effective task management and communication:
+
+    1. Task Analysis: Understand the user's request and identify the main objectives.
+    2. Agent Selection: Choose the most suitable agents for each subtask based on their capabilities.
+    3. Task Delegation: Assign subtasks to the selected agents, ensuring clear communication and expectations.
+    4. Result Synthesis: Collect and integrate the results from all agents, ensuring consistency and coherence.
+    5. Transparency and Reporting: Provide the user with a clear overview of the task progress, including any challenges faced and how they were resolved.
+    6. Change or check configuration: If the task involves network configuration or checking, use the IOS agent to perform these tasks.
+       IOS-agent will know the username and password for the devices.
+    7. ACI Agent: If the task involves ACI configuration, use the ACI agent to perform these tasks.
+    8. Always sending the final result to the via notify-agent, then it will send to user a message via telegram with the structure of content
+
+    NOTE: Please be careful when ask notify-agent, just send the message, don't say too much ask him to send the message.
+
+    Questions from the user:
+    
+    {messages}
+    """
+
+
+supervisor_prompt_template = PromptTemplate(template=supervisor_prompt, input_variables=["messages"])
+
+
+
+
+aci_prompt = """
+    You are a highly experienced CCIE (Cisco Certified Internetwork Expert) with extensive expertise in designing, configuring, and managing complex network Datacenter infrastructures.
+    Your task will check and configure on ACI. Your configure should be valid and executable.
+    The ACI CISCO AGENT will achieve the following objectives:
+    1. Configuration Validation: Check the valid url to call the ACI API and check the configuration for errors, inconsistencies, or deviations from best practices.
+    2. Management: Apply valid, executable ACI configurations based on predefined policies or dynamic requirements.
+    3. Thinking Process: Clearly articulate your thought process and reasoning behind each configuration decision.
+    4. Result Reporting: Provide a detailed report of the configuration changes made, including before-and-after comparisons.
+    
+    Questions to ask:
+    
+    {messages}
+    """
+
+aci_prompt_template = PromptTemplate(template=aci_prompt, input_variables=["messages"])
+
+ios_prompt = """
+    You are a highly experienced CCIE (Cisco Certified Internetwork Expert) with extensive expertise in designing, configuring, and managing complex network infrastructures.
+    Your task will check and configure on Cisco IOS devices. Your configure should be valid and executable.
+    The Network Configuration Agent must achieve the following objectives:
+    1. Configuration Validation: Check Cisco IOS device configurations for errors, inconsistencies, or deviations from best practices.
+    2. Automated Configuration: Apply valid, executable Cisco IOS configurations based on predefined policies or dynamic requirements.
+    3. Thinking Process: Clearly articulate your thought process and reasoning behind each configuration decision.
+    4. Result Reporting: Provide a detailed report of the configuration changes made, including before-and-after comparisons.
+    
+    Some information about the network:
+    Credentials: cisco / cisco
+    Protocol: ssh
+
+    | R1         | IOL XE    | 10.10.20.171 |
+    | R2         | IOL XE    | 10.10.20.172 |
+    | SW1        | IOL L2 XE | 10.10.20.173 |
+    | SW2        | IOL L2 XE | 10.10.20.174 |
+
+    
+    Questions to ask:
+    
+    {messages}
+    """
+
+ios_prompt_template = PromptTemplate(template=ios_prompt, input_variables=["messages"])
+
+notify_prompt = """
+    You are a highly experienced Slack bot designed to notify users about important events and messages.
+    Remember that your channel ID is C086HGY8XAN
+    Question:
+    {messages}
+    """
+
+notify_prompt_template = PromptTemplate(template=notify_prompt, input_variables=["messages"])
+
+###################### END OF PROMPTS ######################
+
+
+# Define the LLM
+llm = ChatGoogleGenerativeAI(api_key=os.getenv("GOOGLE_API_KEY"), model="gemini-2.0-flash-lite", temperature=0.5)
+
+
+@asynccontextmanager
+async def make_graph():
+    client =  MultiServerMCPClient({
+        "aci-agent":{
+            "command": "/home/dsu979/.local/bin/uv",
+            "args" : ["--directory", "/home/dsu979/ACI_MCP/aci_mcp", "run", "/home/dsu979/ACI_MCP/aci_mcp/main.py",],
+            "transport": "stdio"
+        },
+        "ios-agent":{
+            "command": "/home/dsu979/.local/bin/uv",
+            "args" : ["--directory", "/home/dsu979/MCP_Network_automator", "run", "/home/dsu979/MCP_Network_automator/mcp_cisco_server.py"],
+            "transport": "stdio"
+        },
+        "notify-agent":{
+            "command": "npx",
+            "args" : ["-y","@modelcontextprotocol/server-slack"],
+            "env": {
+                "SLACK_BOT_TOKEN": os.getenv("SLACK_BOT_TOKEN", ""),
+                "SLACK_TEAM_ID": os.getenv("SLACK_TEAM_ID", "")
+            },
+            "transport": "stdio"
+        }}) # type: ignore
+        #async with client.session(server_name="aci-agent") as aci_session, client.session(server_name="ios-agent") as aci_session, client.session(server_name="notify-agent") as ios_session, client.session(server_name="notify-agent") as notify_session:
+        # Get the mcp tools
+    aci_mcp_tools = await client.get_tools(server_name="aci-agent")
+    ios_mcp_tools = await client.get_tools(server_name="ios-agent")
+    notify_mcp_tools = await client.get_tools(server_name="notify-agent")
+
+    aci_agent = create_react_agent(
+        model=llm,
+        tools=aci_mcp_tools,
+        name="aci-agent",
+        prompt=aci_prompt_template
+    )
+
+    ios_agent = create_react_agent(
+        model=llm,
+        tools=ios_mcp_tools,
+        name="ios-agent",
+        prompt=notify_prompt_template
+    )
+
+    notify_agent = create_react_agent(
+        model=llm,
+        tools=notify_mcp_tools,
+        name="notify-agent",
+        prompt=notify_prompt_template
+    )
+
+    workflow = create_supervisor(
+        model=llm,
+        agents=[aci_agent,ios_agent,notify_agent],
+        output_mode="full_history",
+        supervisor_name="network-supervisor",
+        prompt=supervisor_prompt_template)
+    
+    # Compile the workflow
+    app = workflow.compile()
+    # Invoke the app with a test message
+    yield app
+
+if __name__ == "__main__":
+    pass
+    
